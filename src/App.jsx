3637eaa39e02ad1fe4d182613d13061d0a35836f
diegotosato <tosato1998@gmail.com>
2025-12-31 13:06:52 +0100

    create array numbers, create root colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 
 function App() {
 
+  const numbers = []
+  for (let i = 1; i <= 90; i++) {
+    const element = i;
+    numbers.push(element)
+  }
+  console.log(numbers);
+
   return (
     <>
       <h1 className="gochi-hand">Web-Tombola</h1>
       <h2 className="montserrat">Montserrat</h2>
+      {
+        numbers.map(num => (
+          <div key={num}>{num}</div>
+        ))
+      }
     </>
   )
 }
